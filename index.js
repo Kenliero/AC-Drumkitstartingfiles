c29dedebc0c3491760e8349c0b1f7afa2872a239
Kenliero <118605983+Kenliero@users.noreply.github.com>
2024-01-04 17:45:38 +0200

    tom played only

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 for (var i = 0; i < document.querySelectorAll(".drum").length;i++)
 {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-    alert("button clicked");
+    var audio= new Audio("sounds/tom-1.mp3");
+    audio.play();
   });
 }
+
+
