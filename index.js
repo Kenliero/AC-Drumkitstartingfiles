a51b5716537c56d2317c750c7bcd45beeffaa552
Kenliero <118605983+Kenliero@users.noreply.github.com>
2024-01-05 11:52:58 +0200

    pressed shade added

--- index.js
+++ index.js
@@ -55,5 +55,9 @@ function callDrums(key) {
 
 function buttonAnimation(currentkey) {
   var activeButton = document.querySelector("." + currentkey);
-  activeButton.classList.add(".pressed");
+  activeButton.classList.add("pressed");
+
+  setTimeout(function() {
+    activeButton.classList.remove("pressed");
+  }, 100);
 }
